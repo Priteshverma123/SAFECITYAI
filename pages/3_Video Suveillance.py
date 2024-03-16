89c146ea0422fd81fd1c5835d3692dc050251e62
Priteshverma123 <Pritesh_verma>
2024-03-16 15:19:13 +0530

    first commit

---
+++ pages/3_Video Suveillance.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SAFECITY-AI",
+    page_icon="👮",
+    layout="wide"
+)
+
+st.title("SAFECITY-AI: LIVE INCIDENT MAP")
+st.sidebar.success("select a  page above")
---
+++ pages/5_About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SAFECITY-AI",
+    page_icon="👮",
+    layout="wide"
+)
+
+st.title("SAFECITY-AI: ABOUT ")
+st.sidebar.success("select a  page above")
